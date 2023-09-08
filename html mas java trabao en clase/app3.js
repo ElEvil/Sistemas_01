a6717e6b4a5707ba5444b3d13cbe9963faf39e03
ElEvil <90443994+ElEvil@users.noreply.github.com>
2023-09-08 12:47:05 -0600

    tarea 5
    tarea 5 del 05/09/2023

---
+++ html mas java trabao en clase/app3.js
@@ -0,0 +1,7 @@
+var nombre="Aron";
+var nombre="Maria";
+console.log(nombre);
+//comandos
+let user="Lucho";
+let user="Juan";
+console.log(user);
---
+++ html mas java trabao en clase/app5.js
@@ -0,0 +1,8 @@
+console.log("Nombre es:");
+console.log(David);
+console.log("Edad es:");
+console.log(23);
+console.log("Su nacionalidad es:");
+console.log(Mexican);
+console.log("Estas comprometodo?");
+console.log(nop);
